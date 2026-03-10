068488e482be47b7dd2e2c2dd8ba3f0dd45bf40f
Tomas Claros <sergio.claros@ucb.edu.bo.com>
2026-03-09 21:35:44 -0400

    Creacion de la pagina del mapa y configuracion de las rutas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,17 @@
-import { MapPageComponent } from './components/map-page/map-page.component';
-
+import { Routes } from '@angular/router';
+import { MapPage } from './components/map-page/map-page';
 export const routes: Routes = [
-  { path: 'mapa', component: MapPageComponent },
-  { path: '', redirectTo: '/mapa', pathMatch: 'full' } // Redirige la raíz al mapa
+  {
+    path: 'mapa',
+    component: MapPage
+  },
+  {
+    path: '',
+    redirectTo: 'mapa',
+    pathMatch: 'full'
+  },
+  {
+    path: '**',
+    redirectTo: 'mapa' // Por si el usuario escribe cualquier cosa en la URL
+  }
 ];
