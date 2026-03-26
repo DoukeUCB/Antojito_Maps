1711189e7f43229df171c05c2fd8e762eedf0402
JhessaelGit <siancas.jhessael@gmail.com>
2026-03-26 15:38:03 -0400

    fix: se agrego las validaciones del registro de restaurante

--- src/app/components/register-restaurant/register-restaurant.component.ts
+++ src/app/components/register-restaurant/register-restaurant.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-register-restaurant',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, FormsModule],
   templateUrl: './register-restaurant.component.html',
   styleUrls: ['./register-restaurant.component.css'],
 })
@@ -12,12 +13,52 @@ export class RegisterRestaurantComponent {
 
   showPassword = false;
 
+  // datos
+  name: string = '';
+  email: string = '';
+  password: string = '';
+
+  // errores
+  errorName: string = '';
+  errorEmail: string = '';
+  errorPassword: string = '';
+
   togglePasswordVisibility() {
     this.showPassword = !this.showPassword;
   }
 
   register() {
-    console.log('Registrando restaurante...');
-    // lógica futura
+    this.clearErrors();
+
+    let valid = true;
+
+    if (!this.name.trim()) {
+      this.errorName = 'El nombre es obligatorio';
+      valid = false;
+    }
+
+    if (!this.email.includes('@')) {
+      this.errorEmail = 'Correo inválido';
+      valid = false;
+    }
+
+    if (this.password.length < 6) {
+      this.errorPassword = 'Mínimo 6 caracteres';
+      valid = false;
+    }
+
+    if (!valid) return;
+
+    console.log('Registro válido:', {
+      name: this.name,
+      email: this.email,
+      password: this.password
+    });
+  }
+
+  clearErrors() {
+    this.errorName = '';
+    this.errorEmail = '';
+    this.errorPassword = '';
   }
 }
