4b1b5191e2f99d0c2e4d2279ba496e42692a430e
Brisheyka Lopez <brisheyka.lopez@ucb.edu.bo>
2026-04-11 02:23:13 -0400

    feat: implementar componentes compartidos app-button y app-card

--- src/app/components/login-restaurant/restaurant-login.component.ts
+++ src/app/components/login-restaurant/restaurant-login.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { Router } from '@angular/router';
-import { LoggerService } from '../../core/services/logger.service';
+import { LoggerService} from '../../core/services/logger.service';
 
 @Component({
   selector: 'app-restaurant-login',
